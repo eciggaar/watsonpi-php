0b96c21973da96c83c2d8710f48566c4eba9e73f
eciggaar <edward@local>
2016-08-05 08:12:58 +0200

    Final changes

--- watson_pi.php
+++ watson_pi.php
@@ -14,56 +14,44 @@
 			<td>
 				<h1 id = "message">Watson Demo PHP application</h1>
 				<h2>Personality Insights response</h2>
-				
 				<pre>
-<?php 
-	// vcap_services Extraction
-	$services_json = json_decode(getenv('VCAP_SERVICES'), true);
-	$watsonPi = $services_json["personality_insights"][0]["credentials"];
+					<?php
+						// VCAP_SERVICES extraction and retrieval of Watson PI credentials
+						//$services_json = json_decode(getenv('VCAP_SERVICES'), true);
+						//$watsonPi = $services_json["<servicename>"][0]["credentials"];
 
-	// Extract the VCAP_SERVICES variables for Watson QA connection.
-	$username = $watsonPi["username"];
-	$password = $watsonPi["password"];
-	$url = $watsonPi["url"] . "/v2/profile";
+						// Extract the VCAP_SERVICES variables for Watson PI connection.
+					  // i.e. username, password and url
+						//$username = '';
+						//$password = '';
+						//$url = '';
 
-	// Curl initialization to make REST call to Watson service
-	$curl = curl_init();
-	
-	// Get textarea field from POST request
-	$data = $_POST["txtarea"];    
-	
-	// Set post arguments for call
-	$post_args = array(
-    	'body' => $data
-	);
-    
-    // Set header arguments for call
-	$header_args = array(
-    	'Content-Type: text/plain',
-    	'Accept: application/json'
-	);
-    
-    // Set options for REST call via curl
-	curl_setopt($curl, CURLOPT_POST, true);
-	curl_setopt($curl, CURLOPT_POSTFIELDS, $post_args);
-	curl_setopt($curl, CURLOPT_HTTPHEADER, $header_args);
-	curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-	curl_setopt($curl, CURLOPT_USERPWD, $username . ":" . $password);
-	curl_setopt($curl, CURLOPT_URL, $url);
-	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+						// Curl initialization to make REST call to Watson service
 
-	// Actual REST call via curl
-	$result = curl_exec($curl);
-    
-	curl_close($curl);
-    
-    // Decode JSON object for furhter usage. Only used here to pretty print JSON object
-	$decoded = json_decode($result, true);
-	
-	echo json_encode($decoded, JSON_PRETTY_PRINT);
-?>
+						// Get textarea field from POST request
+
+						// Set post arguments for call
+
+					  // Set header arguments for call
+
+					  // Set options for REST call via curl
+						//curl_setopt($curl, CURLOPT_POST, true);
+						//curl_setopt($curl, CURLOPT_POSTFIELDS, $post_args);
+						//curl_setopt($curl, CURLOPT_HTTPHEADER, $header_args);
+						//curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+						//curl_setopt($curl, CURLOPT_USERPWD, $username . ":" . $password);
+						//curl_setopt($curl, CURLOPT_URL, $url);
+						//curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+						// Actual REST call via curl and cleanup (closing) of curl call
+
+					  // Decode JSON object for furhter usage. Only used here to pretty print JSON object
+
+					  // Echo result as pretty printed JSON
+
+						echo $_POST["txtarea"];
+					?>
 				</pre>
-				
 			</td>
 		</tr>
 	</table>
